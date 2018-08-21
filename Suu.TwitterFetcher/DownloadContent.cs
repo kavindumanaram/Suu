b49bfe6db50ad4fadffe0b346fe3ed3e00923f0b
Kavindu Manaram <kavindu@calcey.com>
2018-08-21 23:02:29 +0530

    set dropbox api sdk for uploading twiiter propics

--- Suu.TwitterFetcher/DownloadContent.cs
+++ Suu.TwitterFetcher/DownloadContent.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using static System.Net.Mime.MediaTypeNames;
 using System.IO;
 using Suu.FrontEnd.Models;
+using Dropbox.Api;
+using Dropbox.Api.Files;
 
 namespace Suu.TwitterFetcher
 {
@@ -22,26 +24,43 @@ namespace Suu.TwitterFetcher
 
                 foreach (var user in userList)
                 {
-                  
+
                     WebClient client = new WebClient();
                     client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
                     var filePath = AppDomain.CurrentDomain.BaseDirectory + "\\Inserts\\";
                     var extension = Path.GetExtension(user.profile_image_url);
-                    var imageName = $"{user.Id.ToString()}.{extension}";
+                    var imageName = $"{user.Id.ToString()}{extension}";
                     if (!Directory.Exists(filePath))
                     {
                         Directory.CreateDirectory(filePath);
                     }
 
-                    client.DownloadFileAsync(new Uri(user.profile_image_url), $"{filePath}{ imageName}");
+                     client.DownloadFile(new Uri(user.profile_image_url), $"{filePath}{ imageName}");
+                    var dbx = new DropboxClient("bwUjLwfs0zAAAAAAAAAAGmL_efee1PRAMvTqewDyfZeu4eV5iLa677oEzwheG49y");
+                    var t = Task.Run(() => Upload(dbx, "/Suu.TweeterFetcher", imageName, $"{filePath}{ imageName}"));
+                    t.Wait();
+
                 }
             }
-                
+
         }
 
         void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
         {
-             Console.WriteLine("File downloaded");
+            Console.WriteLine("File downloaded");
+        }
+
+
+        async Task Upload(DropboxClient dbx, string folder, string file, string content)
+        {
+            using (var mem = new MemoryStream(File.ReadAllBytes(content)))
+            {
+                var updated = await dbx.Files.UploadAsync(
+                    folder + "/" + file,
+                    WriteMode.Overwrite.Instance,
+                    body: mem);
+                Console.WriteLine("Saved {0}/{1} rev {2}", folder, file, updated.Rev);
+            }
         }
     }
 }
---
+++ Suu.TwitterFetcher/DropboxClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Suu.TwitterFetcher
+{
+    internal class DropboxClient : IDisposable
+    {
+        private string v;
+
+        public DropboxClient(string v)
+        {
+            this.v = v;
+        }
+    }
+}
